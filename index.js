7cca7b24edd0778df1a3b05d21ac62c2999b4d33
pana005 <42758288+pana005@users.noreply.github.com>
2021-03-27 16:23:42 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(function(){
-
+  function call_img(prefix, max_suffix) {
+    var suffix = Math.floor( Math.random() * (max_suffix - 1) + 1 );
+    var img = document.createElement("img");
+    img.src = prefix + '-' + suffix;
+    img.style = "z-index: {cnt}; position: absolute; top: 0; left: 0;"
+    var img_div_position = document.getElementById("box");
+    img_div_position.appendChild(img);
+  }
 
 });
